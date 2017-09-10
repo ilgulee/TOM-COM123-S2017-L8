aa5f891b8ed1af57cc504015ae80c4f90d7b10aa
ilgulee <iglee2000ca@gmail.com>
2017-09-10 16:28:44 -0400

    Editing Person Class

--- TOM-COM123-S2017-L8/Person.cs
+++ TOM-COM123-S2017-L8/Person.cs
@@ -6,8 +6,29 @@ using System.Threading.Tasks;
 
 namespace TOM_COM123_S2017_L8
 {
+    /// <summary>
+    /// This is the Person class
+    /// </summary>
     public class Person
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
 
+        //constructors which takes two arguments
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        //public methods
+        /// <summary>
+        /// This method enables the Person object to talk
+        /// </summary>
+        public void Talks()
+        {
+            Console.WriteLine(this.Name+" is talking");
+        }
     }
 }
